043fad65e1b63408c120fcb6b03b05f4a8f1e466
Malek-Zaag <zaag.malek1@gmail.com>
2023-04-29 11:35:59 +0100

    .

---
+++ Contains Duplicate.py
@@ -0,0 +1,20 @@
+import sys 
+
+
+
+sys.stdin=open("./in.txt","r")
+sys.stdout=open("./out.txt","w")
+
+t=int(input())
+for i in range(t):
+    sett=set()
+    res=False
+    nums=input().split(",")
+    nums=[int(num) for num in nums]
+    for j in nums:
+        if j in sett:
+            res=True
+            break
+        else : 
+            sett.add(j)
+    print(res)
