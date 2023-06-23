975b8eab728337597fdda52b8ac8049d91013d8c
malek <zaag.malek1@gmail.com>
2023-06-23 11:19:31 +0100

    .

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -6,12 +6,10 @@ t=int(input())
 
 for i in range(t):
     k=int(input())
-    tab=input().split(",")
-    tab=[int(k) for k in tab]
+    nums=[int(i) for i in input().split(',')]
     h={}
-    for j in tab:
-        h[j]=h.get(j,0)+1
-    print(h)
-    res=dict(sorted(h.items(), key=lambda item: item[1],reverse=-1))
-    res=list(res.keys()) 
-    print(res[:k])
+    for i in nums:
+        if h.get(i)== None: h[i]=1
+        else : h[i]+=1
+    l=sorted(h.items(),key=lambda x: x[1],reverse=True)
+    print([i[0] for i in l[:k]])
