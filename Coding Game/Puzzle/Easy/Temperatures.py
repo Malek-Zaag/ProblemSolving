5fb480b4758eac1fb8f8ac9f272d592d479fad04
Malek <zaag.malek1@gmail.com>
2023-09-18 18:33:07 +0100

    Update Temperatures.py

--- Coding Game/Puzzle/Easy/Temperatures.py
+++ Coding Game/Puzzle/Easy/Temperatures.py
@@ -10,13 +10,16 @@ if n == 0:
 else : 
     temps=input().split()
     result=int(temps[0])
-    for i in temps:
-        t=int(i)        
-        if abs(int(result)) > abs(t):
-            result=t
-        elif abs(int(result)) == abs(t):
-            result=abs(t)
-    print(result)
+    if len(temps) ==1 : print(result)
+    else :
+        for i in temps[1:]:
+            t=int(i)        
+            if abs(result) > abs(t):
+                result=t
+            elif abs(result) == abs(t) :
+                result=max(result,t)
+        print(result)
+
 
 
 
