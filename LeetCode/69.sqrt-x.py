4ffc20d79119fb6248b1dc44700f25ee6ba8e6fc
Malek <zaag.malek1@gmail.com>
2023-09-23 00:03:39 +0100

    Create 69.sqrt-x.py

---
+++ LeetCode/69.sqrt-x.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+import math
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return (math.floor(math.sqrt(x)))
+# @lc code=end
+
