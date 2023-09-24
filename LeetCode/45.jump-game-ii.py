3821b1994a1c31589467dffccde48b57e3fb784c
Malek <zaag.malek1@gmail.com>
2023-09-24 10:25:10 +0100

    Update 45.jump-game-ii.py

--- LeetCode/45.jump-game-ii.py
+++ LeetCode/45.jump-game-ii.py
@@ -6,23 +6,18 @@
 
 # @lc code=start
 class Solution:
-    def jump(self, nums: List[int]) -> int:
-        if len(nums) == 0  or len(nums) ==1 : return 0
-        rest=len(nums)-1 - nums[0]
-        if rest <= 0:
-            return 1
-        i=1
-        count=1
-        jump_power=nums[0]
-        while rest > 0 and i < len(nums)-1:
-            jump_power-=1
-            if nums[i] < jump_power:
-                i+=1
-            else :
-                jump_power=max(jump_power,nums[i])
-                rest=rest - jump_power
+    def jump(self, nums: List[int]) -> int:     
+        count=0
+        jump_power=0
+        end_jump=0
+        for i in range(len(nums) -1):
+            jump_power=max(jump_power,i+nums[i])
+            if jump_power >= len(nums) -1:
                 count+=1
-                i+=1           
+                break           
+            if i == end_jump :
+                end_jump=jump_power
+                count+=1
         return count   
 # @lc code=end
 
