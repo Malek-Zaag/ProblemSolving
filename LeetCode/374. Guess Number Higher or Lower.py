ac782f93446668e02c53f99ac4884342c3480451
Malek <zaag.malek1@gmail.com>
2023-10-25 16:25:43 +0100

    .

---
+++ LeetCode/374. Guess Number Higher or Lower.py
@@ -0,0 +1,10 @@
+class Solution:
+    def guessNumber(self, n):
+        lo, hi = 1, n
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if guess(mid) == 1:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo
