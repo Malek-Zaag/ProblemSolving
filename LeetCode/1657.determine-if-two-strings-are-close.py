22ae2771830e7444161d60343aeae29a5a5ad6a4
Malek <zaag.malek1@gmail.com>
2024-05-15 22:35:21 +0200

    hashmap :)

---
+++ LeetCode/1657.determine-if-two-strings-are-close.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=1657 lang=python3
+#
+# [1657] Determine if Two Strings Are Close
+#
+
+# @lc code=start
+from itertools import count
+import re
+import collections
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2) : return False
+        count1, count2=collections.Counter(word1),collections.Counter(word2)
+        return True if sorted(list(count1.keys()))==sorted(list(count2.keys())) and sorted(list(count1.values()))==sorted(list(count2.values())) else False
+        
+        
+        
+# @lc code=end
+
