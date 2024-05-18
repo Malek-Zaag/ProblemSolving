36cbc1ecd8ad9bd56a9580a2bf5985f2b72baa21
Malek <zaag.malek1@gmail.com>
2024-05-18 19:37:43 +0200

    linked list optimized sol

--- 2130.maximum-twin-sum-of-a-linked-list.py
+++ 2130.maximum-twin-sum-of-a-linked-list.py
@@ -12,31 +12,45 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        if head == None : return 0
-        if head.next == None: return head.val
-        r=self.getToEnd(head)
-        l=0
-        li=[]
-        curr=head
-        while curr != None:
-            li.append(curr.val)
-            curr=curr.next
-        l=0
-        maxi=0
-        while l < r:
-            maxi=max(li[l]+li[r],maxi)
-            l+=1
-            r-=1
-        return maxi
+    #     if head == None : return 0
+    #     if head.next == None: return head.val
+    #     r=self.getToEnd(head)
+    #     l=0
+    #     li=[]
+    #     curr=head
+    #     while curr != None:
+    #         li.append(curr.val)
+    #         curr=curr.next
+    #     l=0
+    #     maxi=0
+    #     while l < r:
+    #         maxi=max(li[l]+li[r],maxi)
+    #         l+=1
+    #         r-=1
+    #     return maxi
 
-    def getToEnd(self,head):
-        i=0
-        curr=head
-        while curr.next != None:
-            i+=1
-            curr=curr.next
-        return i
-        
+    # def getToEnd(self,head):
+    #     i=0
+    #     curr=head
+    #     while curr.next != None:
+    #         i+=1
+    #         curr=curr.next
+    #     return i
+        fast, slow =head , head 
+        prev=None
+        while fast and fast.next:
+            fast = fast.next.next
+            tmp = slow.next
+            slow.next=prev
+            prev= slow
+            slow = tmp
+        res =0
+        while slow :
+            res = max(res, slow.val + prev.val)
+            slow=slow.next
+            prev=prev.next
+        return res 
+            
         
 # @lc code=end
 
