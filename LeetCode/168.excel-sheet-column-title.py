d9959ea9da9a23c5557c37a567f7859783bf4521
Malek-Zaag <zaag.malek1@gmail.com>
2025-02-02 22:38:58 +0100

    leetcode easy

---
+++ LeetCode/168.excel-sheet-column-title.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=168 lang=python3
+#
+# [168] Excel Sheet Column Title
+#
+
+# @lc code=start
+import string
+from tokenize import String
+
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        output=""
+        while (columnNumber >0):
+            columnNumber-=1
+            output= chr(columnNumber%26 + ord('A'))+ output
+            columnNumber//=26
+        return output
+# @lc code=end
+
