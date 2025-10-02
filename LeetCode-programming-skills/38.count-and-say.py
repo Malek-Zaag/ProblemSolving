7f76da1dd15f73b88ff296a7158902e2c76649d0
Malek <zaag.malek1@gmail.com>
2025-10-02 23:41:06 +0200

    38.count-and-say

--- LeetCode-programming-skills/38.count-and-say.py
+++ LeetCode-programming-skills/38.count-and-say.py
@@ -6,34 +6,19 @@
 
 # @lc code=start
 class Solution:
-    def helper(self,n):
-        res=[]
-        l=len(n)
-        for i in range(0,l-1):
-            count=1
-            mini_res=[1,n[i]]
-            for j in range(i+1,l):
-                if n[j] != n[i]:
-                    res.append(mini_res)
-                    print(mini_res)
-                    break
-                else : 
-                    count+=1
-                    mini_res[0]=count
-                    print(mini_res)
-        return res
-    
-    def converter(self,arr):
-        res=""
-        for i in arr: 
-            res+=i[0] + i[1]
-        return res
-    
     def countAndSay(self, n: int) -> str:
-        result="1"
-        for _ in range(n-1):
-            result=self.converter(self.helper(result))
-        return result
+        res = "1"
+        for _ in range(n - 1):
+            temp, i = "", 0
+            while i < len(res):
+                count = 1
+                while i + 1 < len(res) and res[i] == res[i + 1]:
+                    i += 1
+                    count += 1
+                temp += str(count) + res[i]
+                i += 1
+            res = temp
+        return res
         
 
          
