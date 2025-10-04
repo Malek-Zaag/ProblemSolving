7c723d15ea3a4fa4371d636c2da3bfba21844680
Malek <zaag.malek1@gmail.com>
2025-10-04 14:13:11 +0200

    Number of 1 Bits

--- LeetCode-programming-skills/38.count-and-say.py
+++ LeetCode-programming-skills/38.count-and-say.py
@@ -7,20 +7,6 @@
 # @lc code=start
 class Solution:
     def countAndSay(self, n: int) -> str:
-        res = "1"
-        for _ in range(n - 1):
-            temp, i = "", 0
-            while i < len(res):
-                count = 1
-                while i + 1 < len(res) and res[i] == res[i + 1]:
-                    i += 1
-                    count += 1
-                temp += str(count) + res[i]
-                i += 1
-            res = temp
-        return res
         
-
-         
 # @lc code=end
 
