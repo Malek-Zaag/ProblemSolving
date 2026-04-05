c3b51d865f1bc505d33ce6bf4896350aa07da16b
Malek Zaag <zaag.malek1@gmail.com>
2026-04-05 19:50:40 -0400

    [222] Count Complete Tree Nodes

--- LeetCode/222.count-complete-tree-nodes.py
+++ LeetCode/222.count-complete-tree-nodes.py
@@ -13,6 +13,9 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
         
 # @lc code=end
 
