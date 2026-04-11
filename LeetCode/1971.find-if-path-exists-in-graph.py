95eb1c97e45b1c1c0dc2e032524a50045dc97dfb
Malek Zaag <zaag.malek1@gmail.com>
2026-04-10 22:54:03 -0400

    [1971] Find if Path Exists in Graph

---
+++ LeetCode/1971.find-if-path-exists-in-graph.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=1971 lang=python3
+#
+# [1971] Find if Path Exists in Graph
+#
+
+# @lc code=start
+
+from asyncio import graph
+from collections import defaultdict
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph=defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        print(graph)
+        
+        
+        def dfs(node,visited):
+            if (node == destination):
+                return True
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    if dfs(neighbor,visited):
+                        return True
+            return False      
+        visited=set()
+        return dfs(source,visited)
+
+Solution().validPath(3,[[0,1],[1,2],[2,0]],0,2)
+# @lc code=end
+
---
+++ LeetCode/6.zigzag-conversion.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] Zigzag Conversion
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+# @lc code=end
+
