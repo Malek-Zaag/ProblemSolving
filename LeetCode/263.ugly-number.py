fde0e9587cf4529dde4ea1e51beb985b7b27e25f
Malek Zaag <zaag.malek1@gmail.com>
2026-05-06 18:33:25 -0400

    [263] Ugly Number

---
+++ LeetCode/263.ugly-number.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=263 lang=python3
+#
+# [263] Ugly Number
+#
+
+# @lc code=start
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+        if n % 5 ==0:
+            return self.isUgly(n / 5)
+        elif n % 3 ==0:
+            return self.isUgly( n /3 )
+        elif n % 2==0:
+            return self.isUgly(n /2 )
+        else:
+            return False
+        
+# @lc code=end
+
