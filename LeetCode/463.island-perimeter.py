92123b2378ae4dd44e7a62a9ffb7511fb18e1eb8
Malek Zaag <zaag.malek1@gmail.com>
2026-06-21 18:04:49 -0400

    [463] Island Perimeter

--- LeetCode/463.island-perimeter.py
+++ LeetCode/463.island-perimeter.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        permiter=0 
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] ==  1:
+                    permiter+=4
+                    if (i < len(grid) -1 and grid[i+1][j] == 1): permiter-=2
+                    if (j < len(grid[0]) - 1 and grid[i][j+1] ==1 ): permiter-=2
+        return permiter
         
 # @lc code=end
 
