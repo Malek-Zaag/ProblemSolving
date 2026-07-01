8d6966900f7f9daeace3f0cdf314833cfc5ecc55
Malek Zaag <zaag.malek1@gmail.com>
2026-07-01 11:12:07 -0400

    [500] Keyboard Row

---
+++ LeetCode/500.keyboard-row.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=500 lang=python3
+#
+# [500] Keyboard Row
+#
+
+# @lc code=start
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        l1="qwertyuiop"
+        l2="asdfghjkl"
+        l3="zxcvbnm"
+        res=[]
+        for word in words:
+            w=word.lower()
+            if len(set(l1+w))==len(l1) or len(set(l2+w))==len(l2) or len(set(l3+w))==len(l3) :
+                res.append(word)
+        return res
+
+
+# @lc code=end
+
