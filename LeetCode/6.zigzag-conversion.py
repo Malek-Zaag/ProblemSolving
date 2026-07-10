cc3d281583479d6dfd14446f60c16196397ff45d
Malek Zaag <zaag.malek1@gmail.com>
2026-07-09 21:57:34 -0400

    6] Zigzag Conversion

--- LeetCode/6.zigzag-conversion.py
+++ LeetCode/6.zigzag-conversion.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        if numRows ==1:
+            return s
+        g = [[] for _ in range(numRows)]
+        i,k =0, 1
+        for j in s:
+            g[i].append(j)
+            if (i ==0):
+                k=1
+            elif (i == numRows-1):
+                k=-1
+            i+=k
+        
+        res=""
+        return "".join("".join(row) for row in g)
+                
+            
         
 # @lc code=end
 
